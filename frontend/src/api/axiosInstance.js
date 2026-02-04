621ebf0814208ee90ebd49df9dac324659edc89b
Nithesh <nithesh0628@gmail.com>
2026-02-04 16:37:57 +0530

    feat: Add Axios instance with base URL and an interceptor for attaching authentication tokens.

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // baseURL as per requirement
+    baseURL: 'https://nextcart-4grz.onrender.com/api', // baseURL as per requirement
 });
 
 api.interceptors.request.use((config) => {
